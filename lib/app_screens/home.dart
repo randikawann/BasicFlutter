3d1210ff551bc29d5bd3fd624eb46bd2c354d2af
randikawan <randikawann@gmail.com>
2019-02-17 22:15:10 +0530

    added font and style

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -4,39 +4,19 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-
-    //in this case width and height is not work because of container is the child of Material app widget
-
-//    return Container(
-//      alignment: Alignment.center,
-//      width: 200.0,
-//      height: 100.0,
-//      color: Colors.deepPurple,
-//      child: Text("Flight", textDirection: TextDirection.ltr,),
-//    );
-
-  //this case container is child of center. beacause of it can use width and height
-//    return Center(
-//      child: Container(
-//        alignment: Alignment.center,
-//        width: 200.0,
-//        height: 100.0,
-//        color: Colors.deepPurple,
-//        child: Text("Flight", textDirection: TextDirection.ltr,),
-//      ),
-//    );
-
-  //
         return Container(
       alignment: Alignment.center,
-//      width: 200.0,
-//      height: 100.0,
-
-//      margin: EdgeInsets.all(15.5),
-      margin: EdgeInsets.only(left: 20.0, top:20.0),
-      padding: EdgeInsets.all(17.6),
       color: Colors.deepPurple,
-      child: Text("Flight", textDirection: TextDirection.ltr,),
+      child: Text(
+        "Flight",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          decoration: TextDecoration.none,
+          fontSize: 75,
+
+        ),
+
+      ),
     );
   }
 }
