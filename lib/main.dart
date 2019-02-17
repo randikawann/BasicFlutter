417576a10babad25c2236209810606413adda175
randikawan <randikawann@gmail.com>
2019-02-18 00:17:48 +0530

    added complex list view with generate builder

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,25 @@ void main(){
   );
 }
 
-Widget getListView(){
-  var listView = ListView(
-    children: <Widget>[
+List<String> getListItem(){
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
 
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("LandsCape"),
-        subtitle: Text("Beautiful View ..!!!!"),
-        trailing: Icon(Icons.wb_cloudy),
-        onTap: (){
-          debugPrint("landscape item clicked");
-        },
-      ),
-      ListTile(
-        leading: Icon(Icons.date_range),
-        title: Text("LandsCape"),
-        subtitle: Text("Beautiful View ..!!!!"),
-        trailing: Icon(Icons.wb_incandescent),
-      ),
-      ListTile(
-        leading: Icon(Icons.accessibility),
-        title: Text("LandsCape"),
-        subtitle: Text("Beautiful View ..!!!!"),
-        trailing: Icon(Icons.delete_sweep),
-      )
-    ],
-  );
-  return listView;
+Widget getListView(){
+  var listItems = getListItem();
+  var listview = ListView.builder(
+      itemBuilder: (context, index){
+
+        return ListTile(
+          leading: Icon(Icons.arrow_right),
+          title: Text(listItems[index]),
+          onTap: (){
+            debugPrint('${listItems[index]} was tapped');
+          },
+
+        );
+      }
+   );
+  return listview;
 }
