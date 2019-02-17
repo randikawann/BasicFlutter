cc5db2ad40a9b3171c948d18344fd1949c140b26
randikawan <randikawann@gmail.com>
2019-02-17 21:20:05 +0530

    added logic for souce code

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget{
@@ -8,7 +10,8 @@ class FirstScreen extends StatelessWidget{
         color: Colors.lightBlueAccent,
         child: Center(
           child: Text(
-            "Hello flutter",
+//            "Your lucky number is ${generateLuckyNum()}",
+            generateNum(),
             textDirection: TextDirection.ltr,
             style: TextStyle(color: Colors.white, fontSize: 40.0),
           ),
@@ -16,4 +19,18 @@ class FirstScreen extends StatelessWidget{
     );
   }
 
+
+
+
+//  int generateLuckyNum(){
+//    var random = Random();
+//    int lucky_Num = random.nextInt(10);
+//    return lucky_Num;
+//  }
+
+  String generateNum(){
+    var random = Random();
+    int lucky_Num = random.nextInt(10);
+    return "Your lucky number is $lucky_Num";
+  }
 }
