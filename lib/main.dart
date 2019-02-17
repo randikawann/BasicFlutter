cff3dc6b91bf63e944589ae49f7d81593bacfdb2
randikawan <randikawann@gmail.com>
2019-02-17 21:54:04 +0530

    added container, margin, padding, width

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,11 @@
 import 'package:flutter/material.dart';
-import './app_screens/first_screen.dart';
+import './app_screens/home.dart';
 
-
-//void main(){
-//  runApp(MyFlutterApp());
-//}
-
-//if thre is only one statement it can use flate arrow method
-void main() => runApp(MyFlutterApp());
-
-
-class MyFlutterApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return (
-        MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: "My Flutter app",
-          home:Scaffold(
-            appBar: AppBar(title: Text("First Flutter application"),),
-            body: FirstScreen()
-          ),
-        )
-    );
-  }
-
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Second App",
+      home: Home(),
+    )
+  );
 }
