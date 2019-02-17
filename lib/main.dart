aad5ae9a63ead5bfd9f0aad9a835ca417813c51f
randikawan <randikawann@gmail.com>
2019-02-17 21:01:12 +0530

    added flate arrow function and make statelessclass

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main(){
+//void main(){
+//  runApp(MyFlutterApp());
+//}
 
-  runApp(
+//if thre is only one statement it can use flate arrow method
+void main() => runApp(MyFlutterApp());
 
-    MaterialApp(
-      title: "My Flutter app",
-      home:Scaffold(
-        appBar: AppBar(title: Text("First Flutter application"),),
-        body: Material(
-            color: Colors.lightBlueAccent,
-            child: Center(
-              child: Text(
-                "Hello flutter",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.white, fontSize: 40.0),
-              ),
-            )
-        ),
-      ),
-      )
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return (
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "My Flutter app",
+          home:Scaffold(
+            appBar: AppBar(title: Text("First Flutter application"),),
+            body: Material(
+                color: Colors.lightBlueAccent,
+                child: Center(
+                  child: Text(
+                    "Hello flutter",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(color: Colors.white, fontSize: 40.0),
+                  ),
+                )
+            ),
+          ),
+        )
     );
-
+  }
 
 }
