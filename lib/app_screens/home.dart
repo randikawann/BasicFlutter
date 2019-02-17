312d9a6e9ac57f5e883e0ee59771fe21eb481924
randikawan <randikawann@gmail.com>
2019-02-17 22:59:13 +0530

    added column and rows

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,38 +1,64 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-        return Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      child: Row(
-        children: <Widget>[
-          Expanded(child: Text(
-            "Randika",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 35,
-
+    return Container(
+      padding: EdgeInsets.only(left: 10.0, top: 50.0),
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Randika",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "Kasun Wanninayaka. Sabaragmauwa university of sri lanka",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              ],
             ),
-
-          ),),
-          Expanded(child: Text(
-            "Kasun Wanninayaka. Sabaragmauwa university of sri lanka",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 20,
-
-            ),
-
-          ),)
-
-        ],
-      )
-
-    );
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Kasun",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "Madusanka hettiarachchi. Slit university sri lanka",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
