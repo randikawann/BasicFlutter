6440e0bd5de12ddefcf8cd62db5a260b87a42eb6
randikawan <randikawann@gmail.com>
2019-02-18 17:24:30 +0530

    statue full widget structure

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,112 +1,30 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+//Create statefull widget
+
+class Home extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      padding: EdgeInsets.only(left: 10.0, top: 50.0),
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "Randika",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 35,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "Kasun Wanninayaka. Sabaragmauwa university of sri lanka",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 20,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "Kasun",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 35,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "Madusanka hettiarachchi. Slit university sri lanka",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 20,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            FlightImageAssert(),
-            FlightBookButton()
-          ],
-        ));
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeState();
   }
 }
 
-class FlightImageAssert extends StatelessWidget{
+class _HomeState extends State<Home>{
+  String name = "";
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    AssetImage assetImage = AssetImage("images/29189619.jpeg");
-    Image image = Image(image: assetImage, width: 250.0, height: 250.0,);
-    return Container(
-      margin: EdgeInsets.only(top: 20.0),
-      child: image,
-    );
+    return Column(children: <Widget>[
+      TextField(
+        onSubmitted: (String string){
+          setState(() {
+            name = string;
+          });
+        },
+      ),
+      Text("Your Text is $name ")
+    ],);
   }
-}
 
-class FlightBookButton extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      margin: EdgeInsets.only(top: 20.0),
-      width: 250.0,
-      height: 50.0,
-      child: RaisedButton(
-          color: Colors.deepOrange,
-          child: Text(
-            "Show Details",
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.white,
-          ),),
-          elevation: 6.0,
-          onPressed: () => showUserDetails(context)
-          ),
-    );
-  }
-  void showUserDetails(BuildContext context){
-     var alertDialog = AlertDialog(
-       title: Text("Details shows successfully",),
-       content: Text("Choose like person as soon as posible "),
-     );
-
-     showDialog(context: context,
-     builder: (BuildContext context) => alertDialog
-     );
-  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,53 +8,10 @@ void main(){
       title: "Second App",
       home: Scaffold(
         appBar: AppBar(title: Text("Floating action button"),),
-        body: getListView(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            debugPrint("FAB clicked");
-          },
-          child: Icon(Icons.add),
-          tooltip: 'add one or more item',
-        ),
+        body: Home(),
+
       ),
     )
   );
 }
 
-void showSnacBar(BuildContext context, String item){
-  var snackbar = SnackBar(
-    content: Text("you just clicked $item"),
-    action: SnackBarAction(
-        label: "UNDO",
-        onPressed: (){
-          debugPrint("Performing undo button");
-        }
-    ),
-  );
-
-  Scaffold.of(context).showSnackBar(snackbar);
-}
-
-List<String> getListItem(){
-  var items = List<String>.generate(1000, (counter) => "Item $counter");
-  return items;
-}
-
-Widget getListView(){
-  var listItems = getListItem();
-  var listview = ListView.builder(
-      itemBuilder: (context, index){
-
-        return ListTile(
-          leading: Icon(Icons.arrow_right),
-          title: Text(listItems[index]),
-          onTap: (){
-            showSnacBar(context, listItems[index]);
-            debugPrint('${listItems[index]} was tapped');
-          },
-
-        );
-      }
-   );
-  return listview;
-}
